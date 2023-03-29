646204cd371effdafd650ca54ba8ee95b6ca427f
Daniel Mateu <danielmateu86@gmail.com>
2023-03-29 15:36:52 +0200

    React big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import enUS from 'date-fns/locale/en-US'
+
 import { Navbar } from '../components/Navbar'
+import { addHours, parse, startOfWeek, getDay, format } from 'date-fns'
+
+const locales = {
+    'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
+
+const events = [
+    {
+        title: 'Cumpleaños del jefe',
+        notes: 'Comprar el pastel',
+        start: new Date(),
+        end: addHours(new Date(), 2),
+        bgcolor: '#fafafa',
+        user: {
+            _id: '123',
+            name: 'Dani',
+        }
+    },
+]
 
 const CalendarPage = () => {
     return (
         <>
-            <Navbar/>
+            <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+            />
         </>
     )
 }
