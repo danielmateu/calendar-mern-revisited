e7878aeec110ac89ad56d2a7dd6d56bec31c7534
Daniel Mateu <danielmateu86@gmail.com>
2023-03-29 15:51:03 +0200

    COnfiguraciones adicionales al calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,22 +1,11 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import enUS from 'date-fns/locale/en-US'
-
+import { addHours} from 'date-fns'
 import { Navbar } from '../components/Navbar'
-import { addHours, parse, startOfWeek, getDay, format } from 'date-fns'
 
-const locales = {
-    'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-})
+import { localizer } from '../../helpers/calendarLocalizer'
+import { getMessagesEs } from '../../helpers/getMessages'
 
 const events = [
     {
@@ -33,16 +22,33 @@ const events = [
 ]
 
 const CalendarPage = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        console.log({event, start, end, isSelected});
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        }
+        return {
+            style,
+        }
+    }
     return (
         <>
             <Navbar />
 
             <Calendar
+                culture='es'
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesEs()}
+                eventPropGetter={eventStyleGetter}
             />
         </>
     )
