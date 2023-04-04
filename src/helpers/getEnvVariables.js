e172fdede574cf6e1940837fca9fff31958a53f1
Daniel Mateu <danielmateu86@gmail.com>
2023-04-04 10:34:44 +0200

    app actualizada

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,9 +1,10 @@
 export const getEnvVariables = () => {
 
-    import.meta.env;
+    // import.meta.env;
 
     return {
-        ...import.meta.env,
-        VITE_API_URL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
+        // ...import.meta.env,
+        // VITE_API_URL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
+        VITE_API_URL: import.meta.env.VITE_API_URL,
     };
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { CalendarApp } from './CalendarApp'
 import { store } from './store/store'
 import './index.css'
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <CalendarApp />
+        <CalendarApp />
     </BrowserRouter>
   </Provider>
   // {/* </React.StrictMode>, */}
