0dcb5a59aefcd30f9ca2a2d378762665856e866a
Daniel Mateu <danielmateu86@gmail.com>
2023-04-04 12:18:38 +0200

    Fin tests authSlice

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,5 +1,6 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../__fixtures/authStates";
+import { authSlice, clearErrorMessage, onLogout } from "../../../src/store/auth/authSlice";
+import { authenticatedState, initialState } from "../../__fixtures/authStates";
+import { testUserCredentials } from "../../__fixtures/testUser";
 
 describe('Tests sobre authSlice', () => {
 
@@ -7,5 +8,49 @@ describe('Tests sobre authSlice', () => {
         expect(authSlice.getInitialState()).toEqual(initialState)
     })
 
-    
+    test('Debe de realizar un login', () => {
+        const state = authSlice.reducer(initialState, authSlice.actions.onLogin(testUserCredentials));
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined,
+        })
+    })
+
+    test('Debe de realizar un logout', () => {
+        const state = authSlice.reducer(authenticatedState, onLogout());
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: undefined,
+        })
+    });
+
+    test('Debe de realizar un logout', () => {
+        const errorMessage = 'Credenciales no válidas';
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: errorMessage,
+        })
+    });
+
+    test('Debe de limpiar el mensaje de error', () => {
+
+        const state = authSlice.reducer(authenticatedState, authSlice.actions.clearErrorMessage());
+
+        const newState = authSlice.reducer(state, clearErrorMessage());
+        
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: {
+                uid: '123',
+                name: 'Dani',
+            },
+            errorMessage: undefined,
+        })
+
+        expect(newState.errorMessage).toBe(undefined)
+    })
 })
